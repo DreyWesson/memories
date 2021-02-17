f920a438879b198a5aec27c12eac7644a8f28d05
dreyWesson <dreyWesson@gmail.com>
2021-02-17 08:45:03 +0100

    uninstalled notistack and its implementations

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,11 @@ import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
-import { SnackbarProvider } from "notistack";
 
 ReactDOM.render(
-  // <React.StrictMode>
   <Provider store={store}>
-    <SnackbarProvider maxSnack={3} preventDuplicate dense>
-      <App />
-    </SnackbarProvider>
+    <App />
   </Provider>,
-  // </React.StrictMode>
   document.getElementById("root")
 );
 
